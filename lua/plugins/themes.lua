60f9e15a12dbe0a9850c10e57e80d41b9f36dc94
chriscullins <criscullns@gmail.com>
2026-04-19 09:55:19 -0500

    feat: add extra colorschemes

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "loctvl842/monokai-pro.nvim",
+    lazy = true,
+    opts = {
+      filter = "pro",
+    },
+  },
+  {
+    "Mofiqul/dracula.nvim",
+    lazy = true,
+  },
+  {
+    "sainnhe/sonokai",
+    lazy = true,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = true,
+  },
+  {
+    "EdenEast/nightfox.nvim",
+    lazy = true,
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = true,
+  },
+  {
+    "navarasu/onedark.nvim",
+    lazy = true,
+    opts = {
+      style = "darker",
+    },
+  },
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = true,
+  },
+}
